35b0d375efca12d771980a29f69d812b157c7f3d
henke164 <henrik.mattsson@hotmail.com>
2018-08-29 15:33:41 +0200

    Initial commit

---
+++ src/WoWPal/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WoWPal.EventDispatchers;
+using WoWPal.EventDispatchers.Abstractions;
+
+namespace WoWPal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var eventManager = new EventManager();
+            eventManager.StartEventDispatcher(typeof(ScreenChangedDispatcher));
+
+            eventManager.On("ScreenChanged", (Event ev) => {
+                this.BackgroundImage = (Bitmap)ev.Data;
+            });
+        }
+    }
+}
