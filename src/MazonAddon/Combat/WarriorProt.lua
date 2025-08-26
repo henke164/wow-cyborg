614a8d81a52942d74f45eafb4062efd212f7656c
henke164 <henrik.mattsson@hotmail.com>
2025-08-26 23:47:08 +0200

    prot warrior

--- src/MazonAddon/Combat/WarriorProt.lua
+++ src/MazonAddon/Combat/WarriorProt.lua
@@ -2,11 +2,7 @@
 NAME: Warrior Protection
 ICON: ability_warrior_defensivestance
 ]]--
-local incomingDamage = {}
-local meleeDamageInLast5Seconds = 0
-local rangedDamageInLast5Seconds = 0
 
-local avatar = "F+5";
 local demoralizingShout = "F+6";
 local shieldWall = "F+7";
 local lastStand = "F+8";
@@ -18,16 +14,11 @@ local execute = "4";
 local shieldBlock = "5";
 local ignorePain = "6";
 local victoryRush = "7";
-local attack = "8";
-local battleShout = "CTRL+3";
 local heroicThrow = "0";
 
 local eightYardCheck = 5246;--"Intimidating Shout";
 
 WowCyborg_PAUSE_KEYS = {
-  "F1",
-  "F2",
-  "F3",
   "F4",
   "R",
   "NUMPAD5",
@@ -40,7 +31,7 @@ WowCyborg_PAUSE_KEYS = {
 function GetClapCooldown()
   local sStart, sDuration = GetSpellCooldown("Thunder Clap");
   local tl = sStart + sDuration - GetTime();
-  if tl < 1 then
+  if tl < 0.3 then
     return 0;
   end
 
@@ -52,12 +43,25 @@ function RenderMultiTargetRotation()
 end
 
 function RenderSingleTargetRotation()
+  local maxHp = UnitHealthMax("player");
+  local rage = UnitPower("player");
+  local hpPercentage = GetHealthPercentage("player");
   local nearbyEnemies = GetNearbyEnemyCount(eightYardCheck);
   local targetHp = GetHealthPercentage("target");
   local bsBuff = FindBuff("player", "Battle Shout");  
   local holActive = C_Spell.GetOverrideSpell(6343) == 435222 and GetClapCooldown() == 0;
 
-  if nearbyEnemies > 2 then
+  if (IsCastable("Thunder Clap", 0) or holActive) and nearbyEnemies > 0 then
+    WowCyborg_CURRENTATTACK = "Thunder Clap";
+    return SetSpellRequest(thunderClap);
+  end
+  
+  if hpPercentage < 70 and IsCastableAtEnemyTarget("Impending Victory", 10) then
+    WowCyborg_CURRENTATTACK = "Impending Victory";
+    return SetSpellRequest(victoryRush);
+  end
+
+  if nearbyEnemies > 0 then
     local revBuff = FindBuff("player", "Revenge!");
     if (revBuff ~= nil) then
       if IsCastable("Revenge", 0) then
@@ -67,11 +71,6 @@ function RenderSingleTargetRotation()
     end
   end
 
-  if (IsCastable("Thunder Clap", 0) or holActive) and nearbyEnemies > 0 then
-    WowCyborg_CURRENTATTACK = "Thunder Clap";
-    return SetSpellRequest(thunderClap);
-  end
-  
   if InMeleeRange() == false then
     if InCombatLockdown() and IsCastableAtEnemyTarget("Heroic Throw", 0) then
       WowCyborg_CURRENTATTACK = "Heroic Throw";
@@ -80,17 +79,6 @@ function RenderSingleTargetRotation()
 
     WowCyborg_CURRENTATTACK = "-";
     return SetSpellRequest(nil);
-  end
-
-  local maxHp = UnitHealthMax("player");
-  local rage = UnitPower("player");
-  local hpPercentage = GetHealthPercentage("player");
-  
-  local outburst = FindBuff("player", "Outburst")
-  local vrBuff = FindBuff("player", "Victorious")
-  if hpPercentage < 70 and IsCastableAtEnemyTarget("Impending Victory", 10) then
-    WowCyborg_CURRENTATTACK = "Impending Victory";
-    return SetSpellRequest(victoryRush);
   end
 
   if hpPercentage < 60 then
@@ -110,16 +98,6 @@ function RenderSingleTargetRotation()
     end
   end
 
-  local requiredAmount = maxHp * 0.2;
-    
-  local ipBuff, ipTl, _, __, ___, points = FindBuff("player", "Ignore Pain");
-  local ipAmount = 0;
-  if points ~= nil then
-    ipAmount = points[1]
-  end
-  
-  local needIgnorePain = InCombatLockdown() and (ipBuff == nil or ipTl < 3 or ipAmount < requiredAmount);
-
   if InCombatLockdown() then
     if InMeleeRange() then
       local sbBuff = FindBuff("player", "Shield Block")
@@ -135,47 +113,28 @@ function RenderSingleTargetRotation()
     end
   end
 
-  if nearbyEnemies > 1 then
-    if IsCastable("Thunder Clap", 0) or holActive then
-      WowCyborg_CURRENTATTACK = "Thunder Clap";
-      return SetSpellRequest(thunderClap);
-    end
-
+  if nearbyEnemies > 1 then -- MULTI target
     if IsCastable("Demoralizing Shout", 0) and rage < 70 then
       WowCyborg_CURRENTATTACK = "Demoralizing Shout";
       return SetSpellRequest(demoralizingShout);
     end
 
-    if nearbyEnemies > 4 then
-      if IsCastable("Revenge", 50) and (ipBuff ~= nil and ipTl > 3) then
-        WowCyborg_CURRENTATTACK = "Revenge";
-        return SetSpellRequest(revenge);
-      end
+    if IsCastable("Revenge", 50) and (ipBuff ~= nil and ipTl > 3) then
+      WowCyborg_CURRENTATTACK = "Revenge";
+      return SetSpellRequest(revenge);
     end
 
-    if rage < 50 then
-      if IsCastableAtEnemyTarget("Shield Slam", 0) then
-        WowCyborg_CURRENTATTACK = "Shield Slam";
-        return SetSpellRequest(shieldSlam);
-      end
-    end
-
-    if IsCastable("Thunder Clap", 0) or holActive then
-      WowCyborg_CURRENTATTACK = "Thunder Clap";
-      return SetSpellRequest(thunderClap);
-    end
-    
     if IsCastableAtEnemyTarget("Shield Slam", 0) then
       WowCyborg_CURRENTATTACK = "Shield Slam";
       return SetSpellRequest(shieldSlam);
     end
-  else -- SINGLE target
+  elseif nearbyEnemies == 1 then -- SINGLE target
     if IsCastableAtEnemyTarget("Shield Slam", 0) then
       WowCyborg_CURRENTATTACK = "Shield Slam";
       return SetSpellRequest(shieldSlam);
     end
 
-    if IsCastable("Demoralizing Shout", 0) and (nearbyEnemies > 0 or InMeleeRange() and rage < 70) then
+    if IsCastable("Demoralizing Shout", 0) and rage < 70 then
       WowCyborg_CURRENTATTACK = "Demoralizing Shout";
       return SetSpellRequest(demoralizingShout);
     end
@@ -185,11 +144,6 @@ function RenderSingleTargetRotation()
         WowCyborg_CURRENTATTACK = "Execute";
         return SetSpellRequest(execute);
       end
-    end
-
-    if IsCastable("Thunder Clap", 0) or holActive then
-      WowCyborg_CURRENTATTACK = "Thunder Clap";
-      return SetSpellRequest(thunderClap);
     end
     
     local revBuff = FindBuff("player", "Revenge!");
@@ -201,14 +155,19 @@ function RenderSingleTargetRotation()
     end
   end
 
+  -- Ignore pain logic
+  local requiredAmount = maxHp * 0.2;
+    
+  local ipBuff, ipTl, _, __, ___, points = FindBuff("player", "Ignore Pain");
+  local ipAmount = 0;
+  if points ~= nil then
+    ipAmount = points[1]
+  end
+  
+  local needIgnorePain = InCombatLockdown() and (ipBuff == nil or ipTl < 3 or ipAmount < requiredAmount);
   if needIgnorePain and IsCastable("Ignore Pain", 35) then
     WowCyborg_CURRENTATTACK = "Ignore Pain";
     return SetSpellRequest(ignorePain);
-  end
-  
-  if IsCastableAtEnemyTarget("Shield Slam", 0) then
-    WowCyborg_CURRENTATTACK = "Shield Slam";
-    return SetSpellRequest(shieldSlam);
   end
   
   WowCyborg_CURRENTATTACK = "-";
@@ -219,52 +178,4 @@ function InMeleeRange()
   return IsSpellInRange("Shield Slam", "target") == 1;
 end
 
-function CreateDamageTakenFrame()
-  local frame = CreateFrame("Frame")
-  frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-
-  frame:SetScript("OnEvent", function()
-    local timestamp, type, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, amountDetails = CombatLogGetCurrentEventInfo()
-    if destGUID ~= UnitGUID("player") then
-      return;
-    end
-    
-    local DamageDetails
-    if type == "SPELL_DAMAGE" or type == "SPELL_PERIODIC_DAMAGE" or type == "RANGE_DAMAGE" then
-      _, _, _, damage = amountDetails
-      DamageDetails = { damage = damage, melee = false };
-    elseif type == "SWING_DAMAGE" then
-      damage = amountDetails;
-      DamageDetails = { damage = damage, melee = true };
-    elseif type == "ENVIRONMENTAL_DAMAGE" then
-      _, damage = amountDetails
-      DamageDetails = { damage = damage, melee = false };
-    end
-
-    if DamageDetails and DamageDetails.damage then
-      DamageDetails.timestamp = timestamp;
-
-      tinsert(incomingDamage, 1, DamageDetails);
-
-      local cutoff = timestamp - 5
-      meleeDamageInLast5Seconds = 0
-      rangedDamageInLast5Seconds = 0;
-      for i = #incomingDamage, 1, -1 do
-          local damage = incomingDamage[i]
-          if damage.timestamp < cutoff then
-            incomingDamage[i] = nil
-          else
-            if damage.melee then
-              meleeDamageInLast5Seconds = meleeDamageInLast5Seconds + incomingDamage[i].damage;
-            else
-              rangedDamageInLast5Seconds = rangedDamageInLast5Seconds + incomingDamage[i].damage;
-            end
-          end
-      end
-    end
-
-  end)
-end
-
 print("Protection warrior rotation loaded");
-CreateDamageTakenFrame();
